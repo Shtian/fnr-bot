32f3b1187d2cbdf5b1a9a010ea6814372d944be6
Stian Haga <hagastian@gmail.com>
2020-05-30 11:06:01 +0200

    feat: vercel api endpoint

---
+++ api/get-fnr.ts
@@ -0,0 +1,12 @@
+import { NowRequest, NowResponse } from '@now/node'
+import {generator} from '../src/fnr-generator'
+
+export default (req: NowRequest, res: NowResponse) => {
+    const minAge = req.query.minAge || 0;
+    const maxAge = req.query.maxAge || 120;
+    const numberOfFnrs = req.query.count || 10;
+
+
+    const fnrs = generator(Number(minAge), Number(maxAge), Number(numberOfFnrs));
+    res.json(fnrs);
+}
