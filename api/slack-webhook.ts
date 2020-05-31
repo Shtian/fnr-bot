31112138f268634aad65f6a7fb7a715a4452cac7
Stian Haga <hagastian@gmail.com>
2020-05-31 14:09:56 +0200

    fix: check tokens in slack webhook

--- api/slack-webhook.ts
+++ api/slack-webhook.ts
@@ -20,17 +20,28 @@ const generateBlock = ({ age, gender, fnr }: FnrInfo): SlackSectionBlock => {
 };
 
 export default (req: NowRequest, res: NowResponse): void => {
-  //   console.log(req.body.token, process.env);
-  //   if (req.body.token !== process.env.FNR_SLACK_TOKEN) {
-  //     res.json({
-  //       response_type: "ephemeral",
-  //       text: `Slack token incorrect`,
-  //     });
-  //     return;
-  //   }
+  console.log(req.body.token, process.env);
+  if (
+    !process.env.FNR_SLACK_TOKENS ||
+    !process.env.FNR_SLACK_TOKENS.split(";").includes(req.body.token)
+  ) {
+    res.json({
+      response_type: "ephemeral",
+      text: `Slack token incorrect`,
+    });
+    return;
+  }
 
   // eslint-disable-next-line prefer-const
   let [minAge = "0", maxAge = "120", count = "10"] = req.body.text.split(" ");
+
+  if (minAge > maxAge) {
+    res.json({
+      response_type: "ephemeral",
+      text: `Min age cannot be higher than max age`,
+    });
+    return;
+  }
 
   if (!count) {
     count = 1;
@@ -39,6 +50,7 @@ export default (req: NowRequest, res: NowResponse): void => {
   }
 
   const fnrs = generator(Number(minAge), Number(maxAge), Number(count));
+
   const headerBlock: SlackSectionBlock = {
     type: "section",
     text: {
